e752e50c9b7b6b0125efa4593fef18e1bcaa009d
Quannguyen2541 <quandz.5114@gmail.com>
2024-11-25 15:03:19 +0700

    bai_19_fullupdate+kiemtrasochinhphuong

--- Bai_19-Insonguyen.cpp
+++ Bai_19-Insonguyen.cpp
@@ -25,11 +25,19 @@ bool kiemtrasonguyen(int s) {
 		return false;
 	}
 }
+bool kiemtrasochinhphuong(int p) {
+	if (sqrt(p) == (int)sqrt(p)) return true;
+	return false;
+}
 int main() {
 	int n;
 	cout << "nhap so:\n"; cin >> n;
 	for (int r = 1; r <= n; r++) {
 		if (kiemtrasonguyen(r) == true) cout << r << " ";
+	}
+	for (int i = 1; i < 100; i++)
+	{
+		if (sqrt(i) == (int)sqrt(i)) cout << i << "la so chinh phuong\n";
 	}
 	
 	return 0;
