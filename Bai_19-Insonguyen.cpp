a702bcee9f60ecbb07ca755c49fe275d9a2c971c
Quannguyen2541 <quandz.5114@gmail.com>
2024-11-25 11:05:37 +0700

    bai-19update

--- Bai_19-Insonguyen.cpp
+++ Bai_19-Insonguyen.cpp
@@ -7,6 +7,19 @@ int main() {
 	int n;
 	int dem = 0;
 	cout << "nhap so:\n"; cin >> n;
+	for (int y = 1; y <= n; y++) {
+		dem = 0;
+		for (int i = 1; i <= y; i++)
+		{
+			if (y % i == 0) {
+				dem++;
+
+			}
+		}
+		if (dem == 2) {
+			cout << y << " la so nguyen to ";
+		}
+	}/*
 	for (int i = 1; i <= n; i++)
 	{
 		if (n % i == 0) {
@@ -14,12 +27,13 @@ int main() {
 
 		}
 	}
-	cout << "so dem =" << dem << endl;
+	//cout << "so dem =" << dem << endl;
 	if (dem == 2) {
-		cout << "So" << n << "la so nguyen to\n";
+		cout << "So " << n << " la so nguyen to\n";
 	}
 	else{
-		cout << "So" << n << " khong phai la so nguyen to\n";
-	}
+		cout << "So " << n << " khong phai la so nguyen to\n";
+	}*/
 	return 0;
+
 }
