16b4fb65e11e6f644e760b56f7394a3bd445b9f5
Quannguyen2541 <quandz.5114@gmail.com>
2024-11-25 14:23:50 +0700

    Bai_19_full_update

--- Bai_19-Insonguyen.cpp
+++ Bai_19-Insonguyen.cpp
@@ -3,23 +3,29 @@
 
 #include <iostream>
 using namespace std;
+bool kiemtrasonguyen(int s) {
+	int dem = 0;
+	for (int i = 1; i <= s; i++)
+	{
+		if (s % i == 0) {
+			dem++;
+
+		}
+	}
+	if (dem == 2) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
 int main() {
 	int n;
-	int dem = 0;
 	cout << "nhap so:\n"; cin >> n;
-	for (int y = 1; y <= n; y++) {
-		dem = 0;
-		for (int i = 1; i <= y; i++)
-		{
-			if (y % i == 0) {
-				dem++;
-
-			}
-		}
-		if (dem == 2) {
-			cout << y << " la so nguyen to ";
-		}
-	}/*
+	for (int r = 1; r <= n; r++) {
+		if (kiemtrasonguyen(r) == true) cout << r << " ";
+	}
+	/*
 	for (int i = 1; i <= n; i++)
 	{
 		if (n % i == 0) {
